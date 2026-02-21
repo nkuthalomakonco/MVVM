066da2ce3db7181fc5e77829daea34bd1218a2e0
nkuthalomakonco <nkuthalomakonco@gmail.com>
2026-02-22 00:38:23 +0200

    CommunityToolkit

---
+++ MVVM CommunityToolkit/ViewModel/MainViewModel.cs
@@ -0,0 +1,31 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+
+
+namespace MVVM_CommunityToolkit.ViewModel
+{
+    public partial class MainViewModel : ObservableObject
+    {
+        // Observable property
+        [ObservableProperty]
+        private string newName;
+
+        // Observable collection
+        public ObservableCollection<string> People { get; } = new();
+
+        // Command
+        [RelayCommand]
+        private void AddPerson()
+        {
+            if (!string.IsNullOrWhiteSpace(NewName))
+            {
+                People.Add(NewName);
+                //NewName = string.Empty;
+            }
+        }
+    }
+}
