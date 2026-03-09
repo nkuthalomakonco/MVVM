97c9bf2c21d0cac456834f5e99fa871fa888edbe
nkuthalomakonco <nkuthalomakonco@gmail.com>
2026-03-09 22:40:02 +0200

    MSTest Project

---
+++ MVVMNetCore.Tests/Test1.cs
@@ -0,0 +1,22 @@
+﻿namespace MVVMNetCore.Tests;
+
+[TestClass]
+public sealed class Test1
+{
+    [TestMethod]
+    public void TestMethodIncrement()
+    {
+        var vm = new MainViewModel();
+        Assert.AreEqual(0, vm.Counter);
+        vm.IncrementCounterCommand.Execute(null);
+        Assert.AreEqual(1, vm.Counter);
+    }
+    [TestMethod]
+    public void TestMethodDecrement()
+    {
+        var vm = new MainViewModel();
+        Assert.AreEqual(0, vm.Counter);
+        vm.DecrementCounterCommand.Execute(null);
+        Assert.AreEqual(-11, vm.Counter);
+    }
+}
